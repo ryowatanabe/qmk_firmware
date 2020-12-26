f74bc5b5bdd1bd469528e44abe07eb68bf1343e9
Ryo Watanabe <watanabe.ryo@gmail.com>
2020-12-26 10:48:24 +0900

    enabled mouse keys

--- keyboards/crkbd/rev1/common/keymaps/via/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/via/rules.mk
@@ -1,5 +1,6 @@
-MOUSEKEY_ENABLE     = no     # Mouse keys
+MOUSEKEY_ENABLE     = yes    # Mouse keys
 RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE   = no
 VIA_ENABLE          = yes         # Enable VIA
 OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE          = yes
