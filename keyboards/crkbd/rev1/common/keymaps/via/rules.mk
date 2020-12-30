5505180bfb94c6e87198129db7e8383cbd2fd026
Ryo Watanabe <watanabe.ryo@gmail.com>
2020-12-30 14:59:14 +0900

    enabled audio control

--- keyboards/crkbd/rev1/common/keymaps/via/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/via/rules.mk
@@ -1,6 +1,7 @@
 MOUSEKEY_ENABLE     = yes    # Mouse keys
 RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
 RGB_MATRIX_ENABLE   = no
+EXTRAKEY_ENABLE     = yes
 VIA_ENABLE          = yes         # Enable VIA
 OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE          = yes
